2b7b560569c03b65219c785840b812d49766dd12
AA4085 <aa4085@student.jamk.fi>
2023-05-23 20:15:25 +0300

    path to data folder

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const DATA_FOLDER_PATH = process.env.DATA_FOLDER_PATH
 
 module.exports = {
-    PORT,
-    MONGODB_URI,
-  }
+  PORT,
+  MONGODB_URI,
+  DATA_FOLDER_PATH,
+}
