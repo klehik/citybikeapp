63db2d56286208afeae361ca3199f934cbca1f20
AA4085 <aa4085@student.jamk.fi>
2023-05-30 12:28:18 +0300

    landingpage redirects to /staitons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import Button from '@mui/material/Button'
-
 import Trips from './Trips'
-import { Box, Container, Grid, Typography } from '@mui/material'
-import Map from './Map'
+import { Box } from '@mui/material'
 import Stations from './Stations'
 import Nav from './Nav'
-import { Routes, Route, useMatch, useNavigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 function App() {
   return (
     <Box sx={{ width: 1, height: '100%' }}>
       <Nav></Nav>
       <Box sx={{ width: 1, height: '100%' }}>
         <Routes>
-          <Route path="/stations" element={<Stations></Stations>}></Route>
+          <Route path="/" element={<Navigate replace to="/stations" />} />
+          <Route path="/stations" element={<Stations></Stations>} />
           <Route path="/trips" element={<Trips></Trips>}></Route>
         </Routes>
       </Box>
