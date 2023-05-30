8468c66d7b366dcfd69d8eccd55213392942acc8
AA4085 <aa4085@student.jamk.fi>
2023-05-30 12:58:29 +0300

    morgan logger added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const app = express()
 require('express-async-errors')
 const tripsRouter = require('./routes/trips')
 const stationRouter = require('./routes/stations')
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
 
 app.use('/trips', tripsRouter)
 app.use('/stations', stationRouter)
