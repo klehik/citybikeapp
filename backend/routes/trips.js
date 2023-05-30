0b77e4f378f3bca35b92d5e8568172c085068e85
AA4085 <aa4085@student.jamk.fi>
2023-05-30 14:34:51 +0300

    sorting bt column

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -5,12 +5,15 @@ const Trip = require('../models/Trip')
 router.get('/', async (req, res) => {
   const page = parseInt(req.query.page)
   const limit = parseInt(req.query.limit)
+  const sort = req.query.sort || null
+  const orderby = req.query.orderby === 'asc' ? 1 : -1
   const count = await Trip.collection.estimatedDocumentCount({})
   const pageCount = Math.ceil(count / limit)
 
   const trips = await Trip.find({})
     .skip(limit * page)
     .limit(limit)
+    .sort(sort ? { [`${sort}`]: orderby } : null)
 
   res.status(200).json({ data: trips, hasMore: page < pageCount - 1 })
 })
